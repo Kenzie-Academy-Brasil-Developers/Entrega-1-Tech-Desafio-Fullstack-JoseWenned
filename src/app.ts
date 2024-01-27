c8f22e41bb3136a38ceb8ac9648d48fe30f8c3ce
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-01-27 12:41:06 -0300

    feat: the controllers create and read ready

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import  express, { Application } from "express";
+import { createClientController, readClientController } from "./controllers/client.controller";
 
 export const app: Application = express();
 
 app.use(express.json());
+
+app.post("/clients", createClientController)
+app.get("/clients", readClientController)
