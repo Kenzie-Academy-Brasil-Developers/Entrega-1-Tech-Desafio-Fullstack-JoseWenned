41ac3b653365b3e095082b7d63c7e3886b25bdef
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-02-02 02:03:33 -0300

    feat: interfaces pagination

---
+++ src/interfaces/pagination.interface.ts
@@ -0,0 +1,14 @@
+
+export type Pagination = {
+    prevPage: string | null
+    nextPage: string | null
+    count: number
+    // data: ContactRead
+}
+
+export type PaginationParams = {
+    page: number
+    perPage: number
+    prevPage: string | null
+    nextPage: string | null
+}
