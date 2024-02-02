5aef18e595a41be78d20344f5d816324bf06edfb
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-02-02 13:00:17 -0300

    feat: update line pagination

--- src/middlewares/pagination.middleware.ts
+++ src/middlewares/pagination.middleware.ts
@@ -4,7 +4,7 @@ import { PaginationParams } from "../interfaces/pagination.interface";
 export const pagination = (req: Request, res: Response, next: NextFunction): void => {
     
     const queryPage: number = Number(req.query.page)
-    const queryPerPage: number = Number(req.query.page)
+    const queryPerPage: number = Number(req.query.perPage)
 
     const page: number = queryPage && queryPage > 1 ? queryPage : 1
     const perPage: number = queryPerPage && queryPerPage <= 5 && queryPage > 0 ? queryPerPage : 5
