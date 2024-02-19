f486fb868e4b35b94d214b9ed58df23241f93727
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-02-19 20:36:54 -0300

    feat: ready app

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -2,11 +2,9 @@ import { Router } from "express";
 import { clientRouter } from "./client.router";
 import { contactRouter } from "./contact.router";
 import { loginRouter } from "./login.router";
-import { reportRouter } from "./report.router";
 
 export const routes: Router = Router()
 
 routes.use("/clients", clientRouter)
 routes.use("/contacts", contactRouter)
 routes.use("/login", loginRouter)
-routes.use("/generate-pdf", reportRouter)
